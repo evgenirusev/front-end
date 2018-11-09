86cdb67f8ba51a5923f1030e54060675a8bab134
rusev9 <evg.rusev@gmail.com>
2018-11-09 14:40:17 +0200

    add createRow and deleteRow, cache the table query for better performance, add dynamic variables instead of switch-case

--- javascript-jquery/P01_Countries/script.js
+++ javascript-jquery/P01_Countries/script.js
@@ -1,14 +1,30 @@
-$('.create-country').on('click', () => {
-    let country = $('.country').val();
-    let capital = $('.capital').val();
+// Cache the query for better performance
+let $table = $('table');
 
-    // TODO: refactor readibility
-    let rowToAppend = '<tr><td>' + country + '</td><td>' + capital + '</td><td><button type="submit" class="delete-trigger">Delete</button> <button type="submit" class="down-trigger">Down</button> <button type="submit" class="up-trigger">Up</button></td></tr>';
+let buttonManager = {
+    createRow: function(element) {
+        let tableColumns = element.parentNode.parentNode.children
+        let countryName = tableColumns[0].firstChild.value;
+        let capitalName = tableColumns[1].firstChild.value;
+        let rowToAppend = '<tr><td>' + countryName + '</td><td>' + capitalName + '</td><td><button type="submit" class="deleteRow">Delete</button> <button type="submit" class="downTrigger">Down</button> <button type="submit" class="upTrigger">Up</button></td></tr>';
+        $table.append(rowToAppend);
+    },
 
-    // Append new row
-    $('table tr:last').after(rowToAppend);
+    deleteRow: function(element) {
+        let rowToDelete = element.parentNode.parentNode;
+        $table.children()[0].removeChild(rowToDelete);
+    },
 
-    // TODO: Add event listener for the new dynamically created buttons
+    upTrigger: function(element) {
+        //TODO
+    },
+
+    downTrigger: function(element) {
+        //TODO
+    },
+};
+
+$table.on('click', (event) => {
+    let buttonType = event.target.className;
+    (event.target.tagName == 'BUTTON') ? eval(`buttonManager.${buttonType}(event.target)`) : null;
 });
-
-//TODO : https://stackoverflow.com/questions/34896106/attach-event-to-dynamic-elements-in-javascript
