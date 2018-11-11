4d78c8a942316ebb2d6cd59bb13c25245715181d
rusev9 <evg.rusev@gmail.com>
2018-11-11 21:30:24 +0200

    add funcionality to filter invalid button positions

--- javascript-jquery/P01_Countries/script.js
+++ javascript-jquery/P01_Countries/script.js
@@ -1,12 +1,14 @@
 // Cache the query for better performance
 let $table = $('table');
-
 let buttonManager = {
     createRow: function(element) {
         let tableColumns = element.parentNode.parentNode.children
         let countryName = tableColumns[0].firstChild.value;
         let capitalName = tableColumns[1].firstChild.value;
+
+        //TODO: decouple logic and HTML
         let rowToAppend = '<tr><td>' + countryName + '</td><td>' + capitalName + '</td><td><button type="submit" class="deleteRow">Delete</button> <button type="submit" class="downTrigger">Down</button> <button type="submit" class="upTrigger">Up</button></td></tr>';
+        
         $table.append(rowToAppend);
     },
 
@@ -25,8 +27,6 @@ let buttonManager = {
     upTrigger: function(element) {
         let currentRow = element.parentNode.parentNode;
         let lowerRow = currentRow.previousElementSibling;
-        console.log(currentRow);
-        console.log(lowerRow);
         (lowerRow != null && currentRow != null) ? currentRow.after(lowerRow) : null;
     },
 };
@@ -36,4 +36,17 @@ $table.on('click', (event) => {
 
     // Class name of the clicked button gets mapped to method name in buttonManager
     (event.target.tagName == 'BUTTON') ? eval(`buttonManager.${buttonType}(event.target)`) : null;
+
+    // Fix invalid button positions
+    filterInvalidButtons();
 });
+
+let filterInvalidButtons = function() {
+    // Filter Up button
+    $('table button').css('display', 'inline');
+    $table.children()[0].children[2].getElementsByClassName("upTrigger")[0].style.display = 'none';
+
+    // Filter Down button
+    const tableRows = $('table tr');
+    $(tableRows[tableRows.length - 1]).find('button:contains(\'Down\')').css('display', 'none');
+}
