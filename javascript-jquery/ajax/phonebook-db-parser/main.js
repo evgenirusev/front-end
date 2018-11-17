214980464df6d08558c453a38e3d27609437b2d5
rusev9 <evg.rusev@gmail.com>
2018-11-17 16:55:53 +0200

    add method to dynamically create users through AJAX PATCH REQUEST

--- javascript-jquery/ajax/phonebook-db-parser/main.js
+++ javascript-jquery/ajax/phonebook-db-parser/main.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
     const contactsURL = 'https://fir-app-dd3a7.firebaseio.com/phone-book/contacts/.json';
     $('#loader').click(loadResult);
+    $('#btnCreate').click(createContact);
 
     function loadResult() {
         $('#parsed-results').empty();
@@ -14,6 +15,25 @@ $( document ).ready(function() {
             handleError();
         }
        }); 
+    }
+
+    function createContact() {
+        let name = $('#person').val();
+        let phone = $('#phone').val()
+        let jsonObject = {};
+        jsonObject[name] = phone;
+        let stringifiedJSON = JSON.stringify(jsonObject);
+        $.ajax({
+            type: 'PATCH',
+            data: stringifiedJSON,
+            url: contactsURL,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: loadResult,
+            error: function () {
+                handleError();
+            }
+        })
     }
 
     function displayResults(response) {
