4385679a1d5ce9dc26268faf74440ccc32fccd6d
rusev9 <evg.rusev@gmail.com>
2019-02-27 21:46:57 +0200

    Controlled component

--- react/shopping-cart-ui/src/components/counter.jsx
+++ react/shopping-cart-ui/src/components/counter.jsx
@@ -1,20 +1,12 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.id
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -31,13 +23,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatValue() {
-    const { value } = this.state;
-    return value === 0 ? "Zero" : value;
+    const { value } = this.props.counter;
+    return this.props.counter.value === 0 ? "Zero" : value;
   }
 }
 
--- react/shopping-cart-ui/src/components/counters.jsx
+++ react/shopping-cart-ui/src/components/counters.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
 import Counter from "./counter";
+import { debug } from "util";
 
 class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 2 },
       { id: 2, value: 55 },
-      { id: 3, value: 1 },
-      { id: 4, value: 58 }
-    ]
+      { id: 3, value: 0 },
+      { id: 4, value: 7 }
+    ],
+
+    DEBUG: ["q", "a"]
   };
 
   onDelete = id => {
     const counters = this.state.counters.filter(c => c.id !== id);
+    this.setState({ counters });
+  };
+
+  handleIncrement = counter => {
+    debugger;
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
     this.setState({ counters });
   };
 
@@ -23,6 +35,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.onDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
